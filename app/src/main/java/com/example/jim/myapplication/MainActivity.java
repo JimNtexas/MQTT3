076e6dd0b3adfbcd83dc20e1e372e3e21a92d3b6
Jim Howard <grayraven42@gmail.com>
2018-07-05 13:51:15 -0500

    Publishing works

--- app/src/main/java/com/example/jim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jim/myapplication/MainActivity.java
@@ -23,7 +23,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -34,25 +37,55 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
     private static String TAG = "MQTT MAIN";
     private TextView theMsg;
     MqttClient client = null;
+    private static String topic = "car_command";
+    String clientId = MqttClient.generateClientId();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-      //  setSupportActionBar(toolbar);
-
-        String clientId = MqttClient.generateClientId();
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //  setSupportActionBar(toolbar);
 
 
         final Button chkButton = findViewById(R.id.check_connection);
         chkButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Log.d(TAG,"client connected: " + client.isConnected());
+            public void onClick(View view) {
+                Log.d(TAG, "client connected: " + client.isConnected());
             }
         });
+
+        final Button sendMsg = findViewById(R.id.send_msg);
+        sendMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (client.isConnected()) {
+
+                    MqttMessage message = new MqttMessage("Android calling.".getBytes());
+                    message.setQos(2);
+                    message.setRetained(false);
+
+                    try {
+                        client.publish(topic, message);
+                        Log.i(TAG, "Message published");
+
+                    } catch (MqttPersistenceException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+
+                    } catch (MqttException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    Log.d(TAG, "couldn't send, client disconnected");
+                }
+            }
+
+        });
+
 
         final TextView theMsg = findViewById(R.id.the_msg);
 
@@ -65,32 +98,34 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
             options.setAutomaticReconnect(true);
             client.connect(options);
 
-            String topic = "car_command";
             client.subscribe(topic);
+        } catch (
+                MqttException e1)
 
-        } catch (MqttException e) {
-            e.printStackTrace();
+        {
+            Log.d(TAG, "connect exception: " + e1.toString());
+            e1.printStackTrace();
         }
-
 
     }
 
+
     @Override
     public void connectionLost(Throwable cause) {
-        Log.d(TAG,"connection lost");
+        Log.d(TAG, "connection lost");
     }
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
 
 
-      //  Toast.makeText(MainActivity.this, "Topic: "+topic+"\nMessage: "+message, Toast.LENGTH_LONG).show();
+        //  Toast.makeText(MainActivity.this, "Topic: "+topic+"\nMessage: "+message, Toast.LENGTH_LONG).show();
         Log.d(TAG, "arrived: " + message.toString());
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
-   //     Toast.makeText(MainActivity.this,"delivery complete", Toast.LENGTH_LONG).show();
+        //     Toast.makeText(MainActivity.this,"delivery complete", Toast.LENGTH_LONG).show();
 
     }
 
